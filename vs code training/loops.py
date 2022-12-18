1668e4fb30b98ddb2d5d4249d28c28f38d5b6d5a
BRIAN OTIENO <en2002064@student.mut.ac.ke>
2022-12-18 22:54:42 +0300

    loops commit

---
+++ vs code training/loops.py
@@ -0,0 +1,8 @@
+# Print the numbers from 1 to 5
+for i in range(1, 6):
+  print(i)
+
+# Print the numbers from 1 to 10, but only the even ones
+for i in range(1, 11):
+  if i % 2 == 0:
+    print(i)
