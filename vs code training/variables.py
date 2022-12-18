13f9978b618a05a6171e93d7ef3c6ecaafe4fd6d
BRIAN OTIENO <en2002064@student.mut.ac.ke>
2022-12-18 22:47:44 +0300

    first commit

---
+++ vs code training/variables.py
@@ -0,0 +1,13 @@
+# Assign a value to a variable
+x = 5
+
+# Print the value of the variable
+print(x)
+
+# Re-assign a new value to the variable
+x = 10
+print(x)
+
+# Use the variable in a calculation
+y = x * 2
+print(y)
