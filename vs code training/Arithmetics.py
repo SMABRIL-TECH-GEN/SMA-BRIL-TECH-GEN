e240c96557be46ac375f874aa68dcd331d68a55a
BRIAN OTIENO <en2002064@student.mut.ac.ke>
2022-12-18 22:44:08 +0300

    first commit

---
+++ vs code training/Arithmetics.py
@@ -0,0 +1,13 @@
+# Store a string in a variable
+message = "Hello, World!"
+
+# Store a number in a variable
+x = 10
+y = 5
+
+# Perform arithmetic operations with variables
+sum = x + y
+difference = x - y
+product = x * y
+quotient = x / y
+remainder = x % y
